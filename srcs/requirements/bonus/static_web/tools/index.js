82e03e3bc17552c31e30e6978558976c70a62f59
isromero <ismaromeroortega@gmail.com>
2024-08-06 13:24:22 +0200

    Add static_web with express.js, so finish project

---
+++ srcs/requirements/bonus/static_web/tools/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Static file serving on port ${PORT}`);
+});
