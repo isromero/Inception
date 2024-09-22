c6f74bd5352e3adcfe629d12190a37b141e23bd1
Ismael Romero Ortega <113372543+isromero@users.noreply.github.com>
2024-09-22 19:51:26 +0200

    Fix vsftpd

--- srcs/requirements/bonus/vsftpd/tools/setup_vsftpd.sh
+++ srcs/requirements/bonus/vsftpd/tools/setup_vsftpd.sh
@@ -7,10 +7,15 @@ mkdir -p /var/run/vsftpd/empty
 
 # Add a user and set the password
 if id $FTP_USER > /dev/null 2>&1; then
-    echo "User $FTP_USER already exists"
+  echo "User $FTP_USER already exists"
 else
-  adduser isromero -D -h /home/$FTP_USER/ftp $FTP_USER
-  echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
+adduser isromero -D -h /home/$FTP_USER/ftp $FTP_USER
+echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
 fi
+
+# Create and set permissions for FTP directory
+mkdir -p /home/$FTP_USER/ftp
+chown $FTP_USER:$FTP_USER /home/$FTP_USER/ftp
+chmod 755 /home/$FTP_USER/ftp
 
 exec "$@"
